159f98b4fe879a9bb7464a8ceafb195ac26f9684
aiden.pha <d4in@fresnou.org>
2022-05-05 13:42:46 -0700

    313

---
+++ index.js
@@ -0,0 +1,38 @@
+console.log('i happened')
+const cardList = document.querySelector('.cardList');
+
+addCard('test');
+
+function addCard(value){
+    let card = document.createElement('div');
+    card.classList.add('card');
+    card.classList.add('active');
+    card.innerHTML = value;
+    cardList.appendChild(card);
+}
+
+function buildBoard(){
+    for (let i=0; i<12; i++){
+        addCard('starter');
+    }
+}
+
+cardList.addEventListener('click', function(e){
+    console.log(e.target);
+if (e.target.matches('.cardList')){
+    return
+}
+if (e.target.classList.contains('active')){
+    e.target.classList.remove('active');
+    e.target.classList.add('inactive');
+    }
+e.target.remove();
+});
+
+buildBoard();
+
+let interval = setinteval(function(){
+    addCard( cardList.children.length + 1)//make it start at 1 not 0
+}, 2000);
+
+
